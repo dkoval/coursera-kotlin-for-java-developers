68dae3cb6142a7c6d5f525dd45bb530c4a4daa86
dkoval <koval.dmitriy@gmail.com>
2019-02-17 20:22:16 +0100

    more Kotlin-ish style of coding

--- week2/src/main/kotlin/mastermind/evaluateGuess.kt
+++ week2/src/main/kotlin/mastermind/evaluateGuess.kt
@@ -4,8 +4,6 @@ data class Evaluation(val positions: Int, val letters: Int)
 
 fun evaluateGuess(secret: String, guess: String): Evaluation {
     val positions = secret.zip(guess).count { p -> p.first == p.second }
-    val commonLetters = "ABCDEF".sumBy { ch ->
-        Math.min(secret.count { it -> it == ch }, guess.count { it -> it == ch })
-    }
+    val commonLetters = "ABCDEF".sumBy { ch -> Math.min(secret.count { it == ch }, guess.count { it == ch }) }
     return Evaluation(positions, commonLetters - positions)
 }
