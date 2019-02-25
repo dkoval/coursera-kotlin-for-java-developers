731af291453e435df32f99c196f4981c9b5aed3a
dkoval <koval.dmitriy@gmail.com>
2019-02-25 20:11:49 +0100

    code style

--- week3/src/main/kotlin/nicestring/NiceString.kt
+++ week3/src/main/kotlin/nicestring/NiceString.kt
@@ -2,24 +2,22 @@ package nicestring
 
 fun String.isNice(): Boolean {
     val predicates = listOf(
-            ::doesNotContainPredefinedSubstrings,
-            ::containsAtLeastThreeVowels,
-            ::containsDoubleLetter)
+        ::doesNotContainPredefinedSubstrings,
+        ::containsAtLeastThreeVowels,
+        ::containsDoubleLetter
+    )
     return predicates.count { run(it) } >= 2
 }
 
 private fun String.doesNotContainPredefinedSubstrings(): Boolean {
-    val containsPredefinedStrings =
-            this.contains("bu", true)
+    val containsPredefinedStrings = this.contains("bu", true)
             || this.contains("ba", true)
             || this.contains("be", true)
     return !containsPredefinedStrings
 }
 
 private fun String.containsAtLeastThreeVowels(): Boolean {
-    return this.count { it ->
-        it == 'a' || it == 'e' || it == 'i' || it == 'o' || it == 'u'
-    } >= 3
+    return this.count { it == 'a' || it == 'e' || it == 'i' || it == 'o' || it == 'u' } >= 3
 }
 
 private fun String.containsDoubleLetter(): Boolean {
